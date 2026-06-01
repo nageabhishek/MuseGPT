a77b5351a1eeb4d0c27de73b350e401da8708644
Abhishek Nage <abhisheknage00@gmail.com>
2026-06-01 15:49:32 +0530

    reate a user model

---
+++ src/models/user.model.js
@@ -0,0 +1,32 @@
+const mongoose=require('mongoose')
+
+const userSchema=new mongoose.Schema({
+    fullname:{
+        firstname:{
+            type:String,
+            required:true
+        },
+        lastnane:{
+            type:String,
+            required:true
+        }
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+
+},
+{
+    timestamps:true
+}
+)
+
+const userModel=mongoose.model('user',userSchema)
+
+module.exports=userModel
