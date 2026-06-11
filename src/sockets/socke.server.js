1609042d736b8d92f7f174bb4340d039cb0e96ef
Abhishek Nage <abhisheknage00@gmail.com>
2026-06-11 12:31:11 +0530

    add long term memory

--- src/sockets/socke.server.js
+++ src/sockets/socke.server.js
@@ -1,4 +1,4 @@
-const{ Server, Socket }=require("socket.io")
+const{ Server }=require("socket.io")
 const aiService=require('../services/ai.service')
 const userModel=require('../models/user.model')
 const messageModel=require('../models/messages.model')
@@ -11,8 +11,8 @@ function initSocket(httpServer){
 const io = new Server(httpServer, {});
 
 // middleware 
-io.use(async(Socket,next)=>{
-  const cookies=cookie.parse(Socket.handshake.headers?.cookie ||"")
+io.use(async(socket,next)=>{
+  const cookies=cookie.parse(socket.handshake.headers?.cookie ||"")
 if(!cookies.token){
   next(new Error('Socket Authorization error'))
 }
@@ -20,7 +20,7 @@ try{
   const decoded=jwt.verify(cookies.token,process.env.JWT_KEY)
 
   const user=await userModel.findById(decoded.id)
-   Socket.user=user
+   socket.user=user
 next()
 
 
@@ -42,7 +42,29 @@ io.on("connection", (socket) => {
       content:data.content,
       role:"user"
     })
+
+    const vectors=await aiService.generateVector(data.content)
+    const memory=await queryMemory({
+  queryVector:vectors,
+  limit:3,
+  metadata:{
+    user:socket.user._id
+  }
+})
+
+    await createMemory({
+      vectors,
+      messageId:message._id,
+      metadata:{
+        chat:data.chat,
+        // user:socket.user._id,
+        text:data.content
+      }
+
+
+    })
  
+    
 
  
 
@@ -51,15 +73,49 @@ const chatHistory = (await messageModel.find({
     chat: data.chat
    }).sort({ createdAt: -1 }).limit(20).lean()).reverse();
     
-    const response=await aiService.generateResponce(chatHistory.map(item=>{
-      return {
-        role:item.role,
-        parts:[{text:item.content}]
+
+const stm=chatHistory.map(item=>{
+  return{
+    role:item.role,
+    parts:[{text:item.content}]
+  }
+})
+
+const ltm = [ {
+               role: "user",
+                    parts: [ {
+                        text: `
+
+                        these are some previous messages from the chat, use them to generate a response
+
+                        ${memory.map(item => item.metadata.text).join("\n")}
+ ` } ]} ]
+
+console.log(ltm[0])
+console.log(stm)
+
+    const response=await aiService.generateResponce([...ltm,...stm])
+
+    const responceVector=await aiService.generateVector(response)
+     await createMemory({
+      vectors,
+      messageId:message._id,
+      metadata:{
+        chat:data.chat,
+        user:socket.user._id,
+        text:data.content
       }
-    }))
+
+
+    })
+
+    
+
+
+
     
 // stm memory q
-     await messageModel.create({
+    const responseMessage= await messageModel.create({
       chat:data.chat,
       content:response,
       role:"model"
