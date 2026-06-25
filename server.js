2ca7c255e4b9bbb0c668ff68b6096f23582d7481
Abhishek Nage <abhisheknage00@gmail.com>
2026-06-25 17:31:06 +0530

    updated

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const dns = require('dns');
 // Bypass Fiber ISP DNS, use Google/Cloudflare
 dns.setServers(['8.8.8.8', '1.1.1.1']); 
+const PORT = process.env.PORT || 3000;
 const app=require('./src/app')
 // database
 const connectDb=require('./src/db/db')
@@ -14,6 +15,6 @@ const initSocket=require('./src/sockets/socket.server')
 //calling fnc
 connectDb()
 initSocket(httpServer)
-httpServer.listen(3000,()=>{
+httpServer.listen(PORT,()=>{
     console.log('conneted to server')
 })
